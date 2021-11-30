b002a186c7fc30bed13a1f6c257e9bd281e641fc
Erick Contreras <eri89--@hotmail.com>
2021-11-29 20:10:06 -0600

    Add form to the view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,36 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    # if post.save
+    #   redirect_to action: :index
+    # else
+    #   redirect_to action: :new
+    # end
+
+    # flash[:notice] = 'Post was successfully created' if @post.save respond_with(@post)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to :index, notice: 'Post was successfully created.' }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [:show, :index] do
-    resources :posts, only: [:show, :index]
+    resources :posts, only: [:show, :index, :new, :create]
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
