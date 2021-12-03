480305c3b99a2449016b54865ad97edf3aaed930
Erick Contreras <eri89--@hotmail.com>
2021-12-03 10:31:00 -0600

    Fix old tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { User.new(name: 'bob', photo: '/photo', bio: 'text') }
+
+  before { subject.save }
+
+  it 'name should be present' do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
 end
